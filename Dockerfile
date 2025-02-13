c7a167c89376959c5965a62caede407f13431d84
Adam Wead <awead@users.noreply.github.com>
2025-02-12 23:37:08 -0500

    Creating the murner module and package

--- Dockerfile
+++ Dockerfile
@@ -25,4 +25,4 @@ RUN --mount=type=cache,target=/root/.cache/uv \
 # Place executables in the environment at the front of the path
 ENV PATH="/app/.venv/bin:$PATH"
 
-CMD ["celery", "-A", "tasks", "--workdir", "src", "worker", "--loglevel=info"]
+CMD ["celery", "-A", "murner.tasks", "--workdir", "src/murner", "worker", "--loglevel=info"]
--- src/murner/nrs/api.py
+++ src/murner/nrs/api.py
@@ -1,12 +1,21 @@
+import os
 import requests
 
 class Api:
 
+    KEY = os.getenv("API_KEY", "unknown")
+
     def get_cat_fact():
         url = 'https://catfact.ninja/fact'
-        response = requests.get(url)
+        headers = {
+            "X-API-KEY": Api.KEY,
+        }
+
+        response = requests.get(url, headers=headers)
+
         if response.status_code == 200:
             data = response.json()
             return data["fact"]
         else:
             return None
+
